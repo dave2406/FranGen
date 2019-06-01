b8ab51c21fad7123ad8e6a16cec4e196fe77cf1f
dave2406 <dave2406@hotmail.co.uk>
2019-06-01 17:52:37 +0100

    refactor: Now splits on newline, making namefile more legible

--- cli.js
+++ cli.js
@@ -3,6 +3,6 @@ const fs =require("fs");
 
 fs.readFile(`${__dirname}/names.txt`, (err, data) => { 
     if (err) throw err;
-    var names = data.toString().split(",");
+    var names = data.toString().split(/\r?\n/);
     console.log(names[Math.floor(Math.random()*names.length)]);
 });
