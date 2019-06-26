c3bd7f2974f9ff7e2d243687cd02f9104e227669
Dave <dwithers@scottlogic.com>
2019-06-26 15:16:42 +0100

    feat: allow generation of multiple names

--- cli.js
+++ cli.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 const fs =require("fs");
+const [,, ...args] = process.argv;
 
 fs.readFile(`${__dirname}/names.txt`, (err, data) => { 
     if (err) throw err;
     var names = data.toString().split(/\r?\n/);
-    console.log(names[Math.floor(Math.random()*names.length)]);
-});
+    if (args.length==0){console.log(names[Math.floor(Math.random()*names.length)]);}
+    else if (args[0]>0){
+        for (i=0; i<args[0];i++)
+            console.log(names[Math.floor(Math.random()*names.length)]);
+        }
+    else if (args[0]=="all"){console.log(names)}
+    else console.log("Unrecognised command line option");
+    }
+);
