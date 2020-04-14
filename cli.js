2e8406c7d15a88319d1929afd1f5cd3a999b6600
dave2406 <dave2406@hotmail.co.uk>
2020-04-14 14:39:25 +0100

    feat: add frandom command

--- cli.js
+++ cli.js
@@ -3,6 +3,9 @@ const fs =require("fs");
 var cowsay = require("cowsay");
 const [,, ...args] = process.argv;
 const default_fran = fs.readFileSync("./frans/default.fran", "utf-8")
+const cowboy_fran = fs.readFileSync("./frans/cowboy.fran", "utf-8")
+const astronaut_fran = fs.readFileSync("./frans/astronaut.fran", "utf-8")
+const mario_fran = fs.readFileSync("./frans/mario.fran", "utf-8")
 
 fs.readFile(`${__dirname}/names.txt`, (err, data) => { 
     if (err) throw err;
@@ -14,6 +17,7 @@ fs.readFile(`${__dirname}/names.txt`, (err, data) => {
         }
     else if (args[0]=="all"){console.log(names)}
     else if (args[0]=="cowsay"){console.log(cowsay.say({text : names[Math.floor(Math.random()*names.length)], e : "oO", T : "U "}))}
+    else if (args[0]=="say" && args[1]=="frandom"){console.log([cowboy_fran, astronaut_fran,mario_fran][Math.floor(Math.random() * 3)].replace("$name", names[Math.floor(Math.random()*names.length)]))}
     else if (args[0]=="say"){console.log(default_fran.replace("$name", names[Math.floor(Math.random()*names.length)]))}
     else console.log("Unrecognised command line option");
     }
